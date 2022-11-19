165dbf83fd220e3787a851dcf508b6452e229333
kareemElsedfey <113632609+kareemElsedfey@users.noreply.github.com>
2022-11-19 03:48:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,449 @@
+function openNav(){
+    document.getElementById("mysidenav") .style.width="50%";
+    document.getElementById("main") .style.width="450px";
+  }
+  
+  function closeNav(){
+    document.getElementById("mysidenav") .style.width="0px";
+    document.getElementById("main") .style.width="0px";
+  }
+  function openlevel(){
+    document.getElementById('levels').style.display="block"
+    document.getElementById('play').style.display="none"
+    document.getElementById('rever').style.display="block"
+  }
+  function back(){
+    document.getElementById('levels').style.display="none"
+    document.getElementById('play').style.display="block"
+    document.getElementById('rever').style.display="none"
+    document.getElementById('levelplay').style.display="none"
+    document.getElementById('next').style.display="none"
+  }
+  function openlevel1(){
+    document.getElementById('levelplay').style.display="block"
+    document.getElementById('levels').style.display="none"
+    document.getElementsById('rever').style.display="none"
+    document.getElementById('next').style.display="block"
+  }
+  function trude(){
+    if (true) {
+        document.getElementById('showTrue').style.display="block" 
+        document.getElementById('false').style.display="none"
+        document.getElementById('true').style.display="block"
+        
+    }
+    
+  }
+  function falsee(){
+    if(true){
+        document.getElementById('showTrue').style.display="block"
+        document.getElementById('false').style.display="block"
+        document.getElementById('true').style.display="none"
+       
+    }
+  }
+
+  function trude2(){
+    if (true) {
+        document.getElementById('showTrue2').style.display="block" 
+        document.getElementById('false2').style.display="none"
+        document.getElementById('true2').style.display="block"
+        
+    }
+    
+  }
+  function falsee2(){
+    if(true){
+        document.getElementById('showTrue2').style.display="block"
+        document.getElementById('false2').style.display="block"
+        document.getElementById('true2').style.display="none"
+       
+    }
+  }
+
+  function trude3(){
+    if (true) {
+        document.getElementById('showTrue3').style.display="block" 
+        document.getElementById('false3').style.display="none"
+        document.getElementById('true3').style.display="block"
+    }
+    
+  }
+  function falsee3(){
+    if(true){
+        document.getElementById('showTrue3').style.display="block"
+        document.getElementById('false3').style.display="block"
+        document.getElementById('true3').style.display="none"
+       
+    }
+  }
+
+  function trude4(){
+    if (true) {
+        document.getElementById('showTrue4').style.display="block" 
+        document.getElementById('false4').style.display="none"
+        document.getElementById('true4').style.display="block"
+    }
+    
+  }
+  function falsee4(){
+    if(true){
+        document.getElementById('showTrue4').style.display="block"
+        document.getElementById('false4').style.display="block"
+        document.getElementById('true4').style.display="none"
+       
+    }
+  }
+
+  function trude5(){
+    if (true) {
+        document.getElementById('showTrue5').style.display="block" 
+        document.getElementById('false5').style.display="none"
+        document.getElementById('true5').style.display="block"
+    }
+    
+  }
+  function falsee5(){
+    if(true){
+        document.getElementById('showTrue5').style.display="block"
+        document.getElementById('false5').style.display="block"
+        document.getElementById('true5').style.display="none"
+       
+    }
+  }
+
+  function trude6(){
+    if (true) {
+        document.getElementById('showTrue6').style.display="block" 
+        document.getElementById('false6').style.display="none"
+        document.getElementById('true6').style.display="block"
+    }
+    
+  }
+  function falsee6(){
+    if(true){
+        document.getElementById('showTrue6').style.display="block"
+        document.getElementById('false6').style.display="block"
+        document.getElementById('true6').style.display="none"
+       
+    }
+  }
+
+  function trude7(){
+    if (true) {
+        document.getElementById('showTrue7').style.display="block" 
+        document.getElementById('false7').style.display="none"
+        document.getElementById('true7').style.display="block"
+    }
+    
+  }
+  function falsee7(){
+    if(true){
+        document.getElementById('showTrue7').style.display="block"
+        document.getElementById('false7').style.display="block"
+        document.getElementById('true7').style.display="none"
+       
+    }
+  }
+
+  function trude8(){
+    if (true) {
+        document.getElementById('showTrue8').style.display="block" 
+        document.getElementById('false8').style.display="none"
+        document.getElementById('true8').style.display="block"
+    }
+    
+  }
+  function falsee8(){
+    if(true){
+        document.getElementById('showTrue8').style.display="block"
+        document.getElementById('false8').style.display="block"
+        document.getElementById('true8').style.display="none"
+       
+    }
+  }
+
+  function trude9(){
+    if (true) {
+        document.getElementById('showTrue9').style.display="block" 
+        document.getElementById('false9').style.display="none"
+        document.getElementById('true9').style.display="block"
+    }
+    
+  }
+  function falsee9(){
+    if(true){
+        document.getElementById('showTrue9').style.display="block"
+        document.getElementById('false9').style.display="block"
+        document.getElementById('true9').style.display="none"
+       
+    }
+  }
+
+  function trude10(){
+    if (true) {
+        document.getElementById('showTrue10').style.display="block" 
+        document.getElementById('false10').style.display="none"
+        document.getElementById('true10').style.display="block"
+    }
+    
+  }
+  function falsee10(){
+    if(true){
+        document.getElementById('showTrue10').style.display="block"
+        document.getElementById('false10').style.display="block"
+        document.getElementById('true10').style.display="none"
+       
+    }
+  }
+
+  function trude11(){
+    if (true) {
+        document.getElementById('showTrue11').style.display="block" 
+        document.getElementById('false11').style.display="none"
+        document.getElementById('true11').style.display="block"
+    }
+    
+  }
+  function falsee11(){
+    if(true){
+        document.getElementById('showTrue11').style.display="block"
+        document.getElementById('false11').style.display="block"
+        document.getElementById('true11').style.display="none"
+       
+    }
+  }
+
+  function trude12(){
+    if (true) {
+        document.getElementById('showTrue12').style.display="block" 
+        document.getElementById('false12').style.display="none"
+        document.getElementById('true12').style.display="block"
+    }
+    
+  }
+  function falsee12(){
+    if(true){
+        document.getElementById('showTrue12').style.display="block"
+        document.getElementById('false12').style.display="block"
+        document.getElementById('true12').style.display="none"
+       
+    }
+  }
+
+  function trude13(){
+    if (true) {
+        document.getElementById('showTrue13').style.display="block" 
+        document.getElementById('false13').style.display="none"
+        document.getElementById('true13').style.display="block"
+    }
+    
+  }
+  function falsee13(){
+    if(true){
+        document.getElementById('showTrue13').style.display="block"
+        document.getElementById('false13').style.display="block"
+        document.getElementById('true13').style.display="none"
+       
+    }
+  }
+
+  function trude14(){
+    if (true) {
+        document.getElementById('showTrue14').style.display="block" 
+        document.getElementById('false14').style.display="none"
+        document.getElementById('true14').style.display="block"
+    }
+    
+  }
+  function falsee14(){
+    if(true){
+        document.getElementById('showTrue14').style.display="block"
+        document.getElementById('false14').style.display="block"
+        document.getElementById('true14').style.display="none"
+       
+    }
+  }
+  function trude15(){
+    if (true) {
+        document.getElementById('showTrue15').style.display="block" 
+        document.getElementById('false15').style.display="none"
+        document.getElementById('true15').style.display="block"
+    }
+    
+  }
+  function falsee15(){
+    if(true){
+        document.getElementById('showTrue15').style.display="block"
+        document.getElementById('false15').style.display="block"
+        document.getElementById('true15').style.display="none"
+       
+    }
+  }
+
+  function trude16(){
+    if (true) {
+        document.getElementById('showTrue16').style.display="block" 
+        document.getElementById('false16').style.display="none"
+        document.getElementById('true16').style.display="block"
+    }
+    
+  }
+  function falsee16(){
+    if(true){
+        document.getElementById('showTrue16').style.display="block"
+        document.getElementById('false16').style.display="block"
+        document.getElementById('true16').style.display="none"
+       
+    }
+  }
+
+  function trude17(){
+    if (true) {
+        document.getElementById('showTrue17').style.display="block" 
+        document.getElementById('false17').style.display="none"
+        document.getElementById('true17').style.display="block"
+    }
+    
+  }
+  function falsee17(){
+    if(true){
+        document.getElementById('showTrue17').style.display="block"
+        document.getElementById('false17').style.display="block"
+        document.getElementById('true17').style.display="none"
+       
+    }
+  }
+
+  function trude18(){
+    if (true) {
+        document.getElementById('showTrue18').style.display="block" 
+        document.getElementById('false18').style.display="none"
+        document.getElementById('true18').style.display="block"
+    }
+    
+  }
+  function falsee18(){
+    if(true){
+        document.getElementById('showTrue18').style.display="block"
+        document.getElementById('false18').style.display="block"
+        document.getElementById('true18').style.display="none"
+       
+    }
+  }
+
+  function trude19(){
+    if (true) {
+        document.getElementById('showTrue19').style.display="block" 
+        document.getElementById('false19').style.display="none"
+        document.getElementById('true19').style.display="block"
+    }
+    
+  }
+  function falsee19(){
+    if(true){
+        document.getElementById('showTrue19').style.display="block"
+        document.getElementById('false19').style.display="block"
+        document.getElementById('true19').style.display="none"
+       
+    }
+  }
+
+  function trude20(){
+    if (true) {
+        document.getElementById('showTrue20').style.display="block" 
+        document.getElementById('false20').style.display="none"
+        document.getElementById('true20').style.display="block"
+    }
+    
+  }
+  function falsee20(){
+    if(true){
+        document.getElementById('showTrue20').style.display="block"
+        document.getElementById('false20').style.display="block"
+        document.getElementById('true20').style.display="none"
+       
+    }
+  }
+
+
+  function nextone(){
+    document.getElementById('one').style.display="none"
+    document.getElementById('two').style.display="block"
+  }
+  function nexttwo(){
+    document.getElementById('two').style.display="none"
+    document.getElementById('three').style.display="block"
+  }
+  function nextthree(){
+    document.getElementById('three').style.display="none"
+    document.getElementById('four').style.display="block"
+  }
+  function nextfour(){
+    document.getElementById('four').style.display="none"
+    document.getElementById('five').style.display="block"
+  }
+  function nextfive(){
+    document.getElementById('five').style.display="none"
+    document.getElementById('six').style.display="block"
+  }
+  function nextsix(){
+    document.getElementById('six').style.display="none"
+    document.getElementById('seven').style.display="block"
+  }
+  function nextseven(){
+    document.getElementById('seven').style.display="none"
+    document.getElementById('eight').style.display="block"
+  }
+  function nexteight(){
+    document.getElementById('eight').style.display="none"
+    document.getElementById('nine').style.display="block"
+  }
+  function nextnine(){
+    document.getElementById('nine').style.display="none"
+    document.getElementById('ten').style.display="block"
+  }
+  function nextten(){
+    document.getElementById('ten').style.display="none"
+    document.getElementById('eleven').style.display="block"
+  }
+  function nexteleven(){
+    document.getElementById('eleven').style.display="none"
+    document.getElementById('tweleve').style.display="block"
+  }
+  function nexttweleve(){
+    document.getElementById('tweleve').style.display="none"
+    document.getElementById('threetee').style.display="block"
+  }
+  function nextthreetee(){
+    document.getElementById('threetee').style.display="none"
+    document.getElementById('fourtee').style.display="block"
+  }
+  function nextfourtee(){
+    document.getElementById('fourtee').style.display="none"
+    document.getElementById('fiftee').style.display="block"
+  }
+  function nextfiftee(){
+    document.getElementById('fiftee').style.display="none"
+    document.getElementById('sixtee').style.display="block"
+  }
+   function nextsixtee(){
+    document.getElementById('sixtee').style.display="none"
+    document.getElementById('seventee').style.display="block"
+  }
+  function nextseventee(){
+    document.getElementById('seventee').style.display="none"
+    document.getElementById('eightee').style.display="block"
+  }
+  function nexteightee(){
+    document.getElementById('eightee').style.display="none"
+    document.getElementById('ninetee').style.display="block"
+  }
+  function nextninetee(){
+    document.getElementById('ninetee').style.display="none"
+    document.getElementById('twente').style.display="block"
+  }
+  
+
+  
+  
